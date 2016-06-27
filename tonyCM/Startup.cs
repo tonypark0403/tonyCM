ae19ef1b70385dc6a3942108ed18df3677014321
Administrator <tonypark0403@gmail.com>
2016-06-26 21:09:54 -0400

    Add project files.

---
+++ tonyCM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tonyCM.Startup))]
+namespace tonyCM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
